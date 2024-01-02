755ba18300ef85e5e2055485c37a873df7d2513c
zavx0z <metaversebdfl@gmail.com>
2024-01-02 22:09:05 +0300

    example schema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-class Node extends HTMLElement {
-    constructor(){
-        super()
-        
-    }
+export default class Node extends HTMLElement {
+  /** @type {ShadowRoot} */ #host
+  schema
+  constructor() {
+    super()
+    this.#host = this.attachShadow({ mode: "closed" })
+    this.#host.innerHTML = /*html*/ `<p>Node</p>`
+    console.log(this.#host.innerHTML)
+  }
+  async connectedCallback() {
+    const schema = await fetch(this.getAttribute("schema")).then((data) => data.json())
+    const { title, description, input, output } = schema
+    console.log(title, description, input, output)
+  }
 }
