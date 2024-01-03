c39260ffd90dcf664897019efb8047847d25a1bb
zavx0z <metaversebdfl@gmail.com>
2024-01-03 13:44:11 +0300

    Refactor component structure and update index
    - Remove Node.js component
    - Update Params.js with new modifications
    - Add new SocketString.js component
    - Apply changes to index.js to integrate new structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { ComponentNode } from "./components/Node.js"
 import Params from "./components/Params.js"
 import { i18n } from "./utils.js"
 
@@ -10,10 +9,35 @@ export default class Node extends HTMLElement {
   }
   async connectedCallback() {
     const schema = await fetch(this.getAttribute("schema")).then((data) => data.json())
-    const title = i18n(schema.title)
-    const description = i18n(schema.description)
-    const input = Params(schema.input)
-    const output = Params(schema.output)
-    this.#host.innerHTML = ComponentNode(title, description, input, output)
+
+    const socketTypes = new Set()
+    const input = Params(schema.input, socketTypes)
+    const output = Params(schema.output, socketTypes)
+    for (let socketType of socketTypes) {
+      switch (socketType) {
+        case "string":
+          import("./components/SocketString.js").then((module) => customElements.define("input-string", module.default))
+          break
+        default:
+          break
+      }
+    }
+    this.#host.innerHTML = /*html*/ `
+    <h1 class="node-title">
+      ${i18n(schema.title)} 
+    </h1>
+
+    <p class="node-description">
+      ${i18n(schema.description)}
+    </p>
+
+    <div class="sockets-input">
+      ${input}
+    </div>
+
+    <div class="sockets-output">
+      ${output}
+    </div>
+  `
   }
 }
